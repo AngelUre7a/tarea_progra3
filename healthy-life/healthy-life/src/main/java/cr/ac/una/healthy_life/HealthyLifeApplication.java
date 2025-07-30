69fad45466b04909f6c0a159bda5ca5ac26cfc70
AngelUreña <129628027+AngelUre7a@users.noreply.github.com>
2025-07-30 16:35:52 -0600

    Initialize Commit

---
+++ healthy-life/healthy-life/src/main/java/cr/ac/una/healthy_life/HealthyLifeApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.una.healthy_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyLifeApplication.class, args);
+	}
+
+}
---
+++ healthy-life/healthy-life/src/test/java/cr/ac/una/healthy_life/HealthyLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.una.healthy_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
